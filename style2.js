cea70452ace7fba51c95d5fc824aaede56fb83ae
Dwaipayan Dehury <91361409+Dwaipayan25@users.noreply.github.com>
2022-03-05 23:45:10 +0530

    Add files via upload

---
+++ style2.js
@@ -0,0 +1,17 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+
+    if(username=="dwaipayan01" && password=="12345")
+    {
+        alert("Login Succesfully");
+        
+    }
+    else
+    {
+        alert("Login Failed");
+        
+    }
+
+}
